876d0a291f3209bbd01602a04eb21bf8d74b4403
joepatt98 <56845242+joepatt98@users.noreply.github.com>
2021-03-01 18:32:36 -0600

    Create main.py
    Initial setup and code for main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+def list_functions():
+
+    print("\nPlease type a function name as it is listed below.")
+    print("\nBMI\nRetirement\nExit\n")
+    check_input()
+
+def check_input():
+
+    function = input("Chosen Function: ")
+
+    if (function == "BMI"):
+
+        print("BMI()")
+        list_functions()
+
+    elif (function == "Retirement"):
+
+        print("Retirement()")
+        list_functions()
+
+    elif (function == "Exit"):
+
+        return
+
+    else:
+
+        print("\nA valid input was not detected. Try again.")
+        list_functions()
+
+list_functions()
