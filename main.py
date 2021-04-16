59d42e4f274f84b4b02924cc1f9917c8fbad0c8c
joepatt98 <56845242+joepatt98@users.noreply.github.com>
2021-04-15 19:18:43 -0500

    test

--- main.py
+++ main.py
@@ -2,38 +2,6 @@ from flask import Flask
 import retirement
 import bmi
 
-/*
-def list_functions():
-
-    print("\n*****  Please type a function name as it is listed below.  *****")
-    print("\nBMI\nRetirement\nExit\n")
-    check_input()
-
-def check_input():
-
-    function = input("Chosen Function: ")
-
-    if (function == "BMI"):
-
-        bmi.BMI()
-        list_functions()
-
-    elif (function == "Retirement"):
-
-        retirement.Retirement()
-        list_functions()
-
-    elif (function == "Exit"):
-        print("\n")
-        return
-
-    else:
-
-        print("\nA valid input was not detected. Try again.")
-        list_functions()
-
-list_functions()
-*/
 
 app = Flask(__name__)
 
